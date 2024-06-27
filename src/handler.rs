2aa63695f3a3ab7ae095011919ccbce60c579f37
apurplefrog <theodorebaker08@gmail.com>
2024-06-26 20:54:22 -0400

    reorganize handler.rs

--- src/handler.rs
+++ src/handler.rs
@@ -10,17 +10,21 @@ pub struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, _ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        println!("{} is connected...", ready.user.name);
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
         println!("New message: {}", msg.content);
         println!("\tsent by: {}", msg.author);
-        if msg.content == "discube scramble Mega" {
-            let scramble = &Megaminx::scramble(1)[0];
-            if let Err(why) = msg.channel_id.say(&ctx.http, scramble).await {
+
+        if let Some(reply_message) = reply(&msg.content) {
+            if let Err(why) = msg.channel_id.say(&ctx.http, reply_message).await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
+}
+
+fn reply(command: &str) -> Option<String> {
+    Some(command.to_string())
 }
