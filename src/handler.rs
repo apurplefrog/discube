e75bd81557732ba65b3efb4a07728702e3d6fa6b
apurplefrog <theodorebaker08@gmail.com>
2024-07-23 21:18:38 -0400

    allow multi-message reply to work around discord message length limit

--- src/handler.rs
+++ src/handler.rs
@@ -14,15 +14,17 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
-        if let Some(reply_message) = reply(&msg) {
-            if let Err(why) = msg.channel_id.say(&ctx.http, reply_message).await {
-                println!("Error sending message: {:?}", why);
+        if let Some(reply_messages) = reply(&msg) {
+            for reply_message in reply_messages {
+                if let Err(why) = msg.channel_id.say(&ctx.http, reply_message).await {
+                    println!("Error sending message: {:?}", why);
+                }
             }
         }
     }
 }
 
-fn reply(msg: &Message) -> Option<String> {
+fn reply(msg: &Message) -> Option<Vec<String>> {
     let command_words: Vec<&str> = msg.content.split_whitespace().collect();
     if command_words[0] != "discube" {
         return None;
@@ -31,7 +33,7 @@ fn reply(msg: &Message) -> Option<String> {
     Some(command(msg))
 }
 
-fn command(msg: &Message) -> String {
+fn command(msg: &Message) -> Vec<String> {
     let command_words = &msg.content.split_whitespace().collect::<Vec<&str>>()[1..];
     let function = command_words[0];
 
@@ -40,10 +42,10 @@ fn command(msg: &Message) -> String {
             if let Ok(number) = command_words[1].parse::<u32>() {
                 generate_daily_scrambles(number)
             } else {
-                "must be a valid number lmao".to_string()
+                vec!["must be a valid number lmao".to_string()]
             }
         }
-        _ => format!("Unknown function: {}", function),
+        _ => vec![format!("Unknown function: {}", function)],
     }
 }
 
@@ -55,7 +57,7 @@ fn format_scramble_vector(scramble_vector: Vec<String>) -> String {
         .collect::<String>()
 }
 
-fn generate_daily_scrambles(number: u32) -> String {
+fn generate_daily_scrambles(number: u32) -> Vec<String> {
     //let cube1 = Cube::random_cube();
     let cube1 = Cube::Megaminx;
     //let cube2 = Cube::random_cube();
@@ -64,16 +66,22 @@ fn generate_daily_scrambles(number: u32) -> String {
     let scrambles1 = cube1.scramble(cube1.average_scramble_count());
     let scrambles2 = cube2.scramble(cube2.average_scramble_count());
 
-    let format = format!(
-        "# Daily Competition #{}! 🔥\n\n**{}** Scrambles:\n{}\n\n**{}** Scrambles:\n{}",
+    let intro = format!(
+        "# Daily Comp #{} 🔥\nToday's events are **{}** and **{}**",
         number,
         cube1.long_name(),
-        format_scramble_vector(scrambles1),
+        cube2.long_name()
+    );
+    let formatted_scrambles1 = format!(
+        "**{}** Scrambles:\n{}",
+        cube1.long_name(),
+        format_scramble_vector(scrambles1)
+    );
+    let formatted_scrambles2 = format!(
+        "**{}** Scrambles:\n{}",
         cube2.long_name(),
-        format_scramble_vector(scrambles2),
+        format_scramble_vector(scrambles2)
     );
 
-    println!("{format}");
-    println!("LENGTH: {}", format.len());
-    format
+    vec![intro, formatted_scrambles1, formatted_scrambles2]
 }
